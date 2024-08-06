2ff48b1ce54c3e49c73fd489521b14c8ec77f733
DeseoApartments <info@deseoapartments.com>
2024-08-06 14:46:51 +0200

    cxasa

--- scripts.js
+++ scripts.js
@@ -5,5 +5,35 @@ function scrollToElement(imageName) {
     }
 }
 
+function toggleMenu() {
+    const menuList = document.querySelector('.menu-list');
+    menuList.classList.toggle('active');
+}
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+const gallery = document.querySelector('.gallery');
+
+function showSlides(n) {
+if (n >= slides.length) {
+slideIndex = 0;
+} else if (n < 0) {
+slideIndex = slides.length - 1;
+} else {
+slideIndex = n;
+}
+
+const offset = -slideIndex * 100;
+gallery.style.transform = `translateX(${offset}%)`;
+}
+
+function plusSlides(n) {
+showSlides(slideIndex + n);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+showSlides(slideIndex);
+});
+
   
 
