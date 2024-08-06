8654c669b238e29757cafab830b15aed3fa65647
DeseoApartments <info@deseoapartments.com>
2024-08-06 16:24:20 +0200

    asd

--- scripts.js
+++ scripts.js
@@ -10,52 +10,39 @@ function toggleMenu() {
     menuList.classList.toggle('active');
 }
 
-let slideIndex = 0;
-const slides = document.getElementsByClassName("slide");
-const gallery = document.querySelector('.gallery');
-
-function showSlides(n) {
-if (n >= slides.length) {
-slideIndex = 0;
-} else if (n < 0) {
-slideIndex = slides.length - 1;
-} else {
-slideIndex = n;
-}
-
-const offset = -slideIndex * 100;
-gallery.style.transform = `translateX(${offset}%)`;
-}
-
-function plusSlides(n) {
-showSlides(slideIndex + n);
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-showSlides(slideIndex);
+    const galleries = document.querySelectorAll('.gallery-container');
+
+    galleries.forEach(galleryContainer => {
+        let slideIndex = 0;
+        const gallery = galleryContainer.querySelector('.gallery');
+        const slides = gallery.getElementsByClassName("slide");
+
+        function showSlides(n) {
+            if (n >= slides.length) {
+                slideIndex = 0;
+            } else if (n < 0) {
+                slideIndex = slides.length - 1;
+            } else {
+                slideIndex = n;
+            }
+
+            const offset = -slideIndex * 100;
+            gallery.style.transform = `translateX(${offset}%)`;
+        }
+
+        function plusSlides(n) {
+            showSlides(slideIndex + n);
+        }
+
+        galleryContainer.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+        galleryContainer.querySelector('.next').addEventListener('click', () => plusSlides(1));
+
+        showSlides(slideIndex);
+    });
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
 
   
 
