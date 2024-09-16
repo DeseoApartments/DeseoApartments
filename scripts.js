0aa8a5ee34336bedc33893076b35d91c6b67275c
DeseoApartments <info@deseoapartments.com>
2024-09-16 12:43:30 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -188,6 +188,18 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Funzione per cancellare tutti i cookies
+function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
+
 
 
 }
